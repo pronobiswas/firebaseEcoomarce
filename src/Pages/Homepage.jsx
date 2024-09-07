8343cd31b34ea07f1dc0808a405f87dec93b9eb8
PRONOB BISWAS <biswaspronob@outlook.com>
2024-09-08 03:04:45 +0600

    update and create post page and use foormik but yup has not norking

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 
+
 const Homepage = () => {
   return (
     <div className='w-full max-w-[1200px] h-[500px] mx-auto px-5 flex'>
 
-      <div className="filterMenu w-1/6 h-full bg-slate-500">
-        <h2>the filter</h2>
+      <div className="filterMenu w-1/6 h-full bg-slate-200">
+        <h2 className='text-2xl'>the filter</h2>
+        <div>
+          <details>
+            <summary>basaVara</summary>
+            <p>Bachelor</p>
+            <p>Family</p>
+          </details>
+          <details>
+            <summary>dokanVara</summary>
+            <p>dokanVara</p>
+          </details>
+          <details>
+            <summary>gariVara</summary>
+            <p>bus</p>
+            <p>truck</p>
+            <p>picup</p>
+            <p>hiex</p>
+          </details>
+          <details>
+            <summary>gariVara</summary>
+            <p>gariVara</p>
+          </details>
+        </div>
+        <div>
+          <details>
+            <summary>By division</summary>
+            <p>khulna</p>
+            <p>khulna</p>
+          </details>
+        </div>
       </div>
 
       <div className="contentSection w-4/6 h-full bg-slate-300">
--- src/Pages/PostPage.jsx
+++ src/Pages/PostPage.jsx
@@ -1,33 +1,109 @@
 import React from 'react'
+import { Formik, useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const PostPage = () => {
-  return (
-    <div>
-      <h2>post your service with aquerecy</h2>
-      <div>
-        <form>
-            <div className="username">
-                <label htmlFor="username">Your Name</label>
-                <input type="text" placeholder='Enter your full Name' />
-            </div>
-            <div className="userEmail">
-                <label htmlFor="userEmail">Your email</label>
-                <input type="text" placeholder='Enter your Email' />
-            </div>
-            <div className="postType">
-                <select name="postType" id="postType">
-                    <option value="basaVara">basa vara</option>
-                    <option value="gariVara">gari vara</option>
-                </select>
-            </div>
-            <div className="locaion">
-                <label htmlFor="username">Your location</label>
-                <input type="text" placeholder='Enter your full Location' />
-            </div>
 
-        </form>
+  const emailRegx = "^[A-Za-z0-9](([a-zA-Z0-9,=\.!\-#|\$%\^&\*\+/\?_`\{\}~]+)*)@(?:[0-9a-zA-Z-]+\.)+[a-zA-Z]{2,9}$"
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      userEmail: '',
+      postType: '',
+      locaion: '',
+      decription: ''
+    },
+
+    // validationSchema: Yup.object({
+    
+    //   username: Yup.string()
+    //   .max(12,' username max 12 charecter')
+    //   .required('Required'),
+
+    //   userEmail: Yup.string()
+    //   .email('Invalid email address')
+    //   .matches(emailRegx , "Enter Your Full mail")
+    //   .required('Required'),
+
+    //   postType: Yup.string()
+    //   .required('Required'),
+
+    //   locaion: Yup.string()
+    //   .max(12,' you must enter your location')
+    //   .required('Required'),
+
+    //   decription: Yup.string()
+    //   .max(220,' you must enter your decription')
+    //   .required('Required'),
+
+    // }),
+
+      onSubmit: values => {
+        // alert(JSON.stringify(values, null, 2));
+        console.log(values);
+        
+      },
+    });
+  return (
+    <>
+      <div className='w-full max-w-[1200px] mx-auto px-5'>
+        <h2 className='text-2xl'>post your service with aquerecy</h2>
+        <div className='bg-red-300 w-full max-w-[480px] mx-auto px-5 py-8'>
+          <form className='' onSubmit={formik.handleSubmit}>
+            <div className='flex flex-col gap-4'>
+              <div className="username inputBox">
+                  <label htmlFor="username">Your Name</label>
+                  <input 
+                  type="text" 
+                  id="username"
+                  name="username"
+                  onChange={formik.handleChange}
+                  value={formik.values.username}
+                  placeholder='Enter your full Name' />
+                  {formik.touched.username && formik.errors.username ? (<div>{formik.errors.username}</div>) : null}
+              </div>
+              <div className="userEmail inputBox">
+                  <label htmlFor="userEmail">Your email</label>
+                  <input 
+                  type="email" 
+                  id="userEmail"
+                  name="userEmail"
+                  onChange={formik.handleChange}
+                  value={formik.values.userEmail}
+                  placeholder='Enter your Email' />
+              </div>
+              <div className="postType inputBox">
+                  <select name="postType" id="postType" onChange={formik.handleChange} value={formik.values.postType}>
+                      <option value="basaVara">basa vara</option>
+                      <option value="gariVara">gari vara</option>
+                  </select>
+              </div>
+              <div className="locaion inputBox">
+                  <label htmlFor="locaion">Your location</label>
+                  <input 
+                  type="text" 
+                  id='locaion' 
+                  name='locaion' 
+                  onChange={formik.handleChange}
+                  value={formik.values.locaion}
+                  placeholder='Enter your full Location' />
+              </div>
+              <div className="decription inputBox">
+                  <label htmlFor="decription">Your location</label>
+                  <textarea 
+                  name="decription" 
+                  id="decription"
+                  onChange={formik.handleChange}
+                  value={formik.values.decription}
+                  placeholder='Enter your full Location'></textarea>
+              </div>
+            </div>
+            <button type="submit">post</button>
+
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
